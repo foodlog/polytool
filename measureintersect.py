8d0ce052b0c97500ed8952a2c92d84cca472be5b
louisdavcren <lejdc15@gmail.com>
2019-08-01 11:32:42 +0100

    equalise arrays

--- measureintersect.py
+++ measureintersect.py
@@ -10,11 +10,11 @@ AmaxY = max(polygonA[1::2])
 AminX = min(polygonA[::2])
 AminY = min(polygonA[1::2])
 
-print("Largest X co-ordonate = " + str(AmaxX))
-print("Largest Y co-ordonate = " + str(AmaxY))
-
-print("Smallest X co-ordonate = " + str(AminX))
-print("Smallest Y co-ordonate = " + str(AminY))
+# print("Largest X co-ordonate = " + str(AmaxX))
+# print("Largest Y co-ordonate = " + str(AmaxY))
+#
+# print("Smallest X co-ordonate = " + str(AminX))
+# print("Smallest Y co-ordonate = " + str(AminY))
 
 # Bbox Corner dimensions (Clockwise order)
 A_topleft = (AminX, AmaxY)
@@ -22,10 +22,8 @@ A_topright = (AmaxX, AmaxY)
 A_bottomright = (AmaxX, AminY)
 A_bottomleft = (AminX, AminY)
 
-print(A_topleft)
-print(A_topright)
-print(A_bottomright)
-print(A_bottomleft)
+print("Bbox A : " + str(A_topleft) + ", " + str(A_topright) + ", " + str(A_bottomright) + ", " + str(A_bottomleft))
+print("Bbox B : ")
 
 A_length = len(polygonA)
 B_length = len(polygonB)
@@ -37,12 +35,17 @@ def equalize_dimensions(polygonA, polygonB, A_length, B_length):
 
     difference = (A_length - B_length)
     # while(A_length != B_length):
-    while(A_length > B_length):
-        M = ((polygonA[0] + polygonA[-2])/difference, (polygonA[1] + polygonA[-1])/difference)
-        return M
 
+    while(difference != 0):
+        M = ((polygonA[0] + polygonA[-2])/difference, (polygonA[1] + polygonA[-1])/difference)
+        difference -= 1
+        polygonB.extend(M)
+        return M, polygonA
 
 equalize_dimensions(polygonA, polygonB, A_length, B_length)
 
-print(M)
+print("polygonA : " + str(polygonA))
+print("polygonB : " + str(polygonB))
+
+print("Midpoint : " + str(M))
 print("Difference : " + str(abs(difference)))
